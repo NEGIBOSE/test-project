d962869be3b4183e2f93291192567b9fda464b96
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-04-19 14:59:47 +0900

    cssまで戻す

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -55,15 +55,15 @@
 <script>
     window.onload = function () {
         var images = [
-        "/images/picture1.png",
-        "/images/picture2.png",
-        "/images/picture3.png",
-        "/images/picture4.png",
-        "/images/picture5.png",
-        "/images/picture6.png",
-        "/images/picture7.png",
-        "/images/picture8.png",
-        "/images/picture9.png",
+        "./images/picture1.png",
+        "./images/picture2.png",
+        "./images/picture3.png",
+        "./images/picture4.png",
+        "./images/picture5.png",
+        "./images/picture6.png",
+        "./images/picture7.png",
+        "./images/picture8.png",
+        "./images/picture9.png",
     ];
 
   var randomIndex = Math.floor(Math.random() * images.length);
