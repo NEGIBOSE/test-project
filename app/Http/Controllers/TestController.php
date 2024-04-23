18b7950af0c92f906036c4ee9404424a8ba325b2
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-04-23 14:03:39 +0900

    p170

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,6 +9,8 @@ class TestController extends Controller
 {
     public function test() {
         $users = User::all();
-        return view('test');
+        return view('test', compact('users'));
+        // return view('test', ['users'=> $users]); 配列で渡す場合
+        // return view('test')->with('users', $users); with関数を使う場合
     }
 }
