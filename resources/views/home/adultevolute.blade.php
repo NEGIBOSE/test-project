7d4d34b330bfaeb9751474e9d56d2dc8cccfa33b
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-06-13 17:52:02 +0900

    第二進化記載

--- resources/views/home/adultevolute.blade.php
+++ resources/views/home/adultevolute.blade.php
@@ -24,6 +24,7 @@
     </header>
     <main class="evolute bg_red">
       <div class="mini_title">
+        第二進化
         <p id="comment" class="select_text fade-in-comment"></p>
       </div>
       <div class="evolution_girl">
