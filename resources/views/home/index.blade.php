bc1c2276ba15615401bc05a308688afe74f5520d
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-06-19 23:58:12 +0900

    再会

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -48,7 +48,7 @@
     <div class="home_select">
         <div class="home_select_u">
             <div class="select_text bg_red">
-                <a href="{{ route('home.search') }}">読み聞かせ</a>
+                <a href="{{ route('home.search') }}">読み聞かせ88</a>
             </div>
             <div class="select_text bg_yellow">
                 <a href="{{ route('home.bookshelf') }}">本棚</a>
