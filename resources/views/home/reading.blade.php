8fbb5e5a2e2ea14bd0c01f4bab9da941f2b80577
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-06-27 23:59:48 +0900

    ４４

--- resources/views/home/reading.blade.php
+++ resources/views/home/reading.blade.php
@@ -82,7 +82,7 @@
 </head>
   <body>
     <header class="header bg_red">
-      <div class="title">Chara-Booksっq</div>
+      <div class="title">Chara-Books</div>
     </header>
     <main class="reading bg_red">
       <div class="mini_title">
