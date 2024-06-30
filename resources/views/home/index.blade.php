57497e65b5c2c9157c09c8ceed35dfc50402429b
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-06-30 22:28:47 +0900

    chra_pic　ホバーアクション

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -25,18 +25,10 @@
             border-radius: 16px;
             overflow: hidden;
             position: relative;
-            box-shadow:0px 0px 4px 4px rgba(255, 255, 255, 1), 
-                        0px 0px 8px 8px rgba(255, 255, 255, 1);
-            transition: transform 0.3s, box-shadow 0.3s;
         }
         .home_pic img {
             width: 100%;
             display: block;
-        }
-        .home_pic:hover {
-            transform: translateY(-5px);
-            box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2), 
-                        0 12px 24px rgba(0, 0, 0, 0.2);
         }
     </style>
 </head>
