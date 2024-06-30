43a8c1a04cb0d49f7d3dc6634aa11ec0d27c1a99
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-06-30 22:11:58 +0900

    chara_pic,　奥行き

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -21,6 +21,23 @@
         .home_icon svg {
             width: 28px;
         }
+        .home_pic {
+            border-radius: 16px;
+            overflow: hidden;
+            position: relative;
+            box-shadow:0px 0px 4px 4px rgba(255, 255, 255, 1), 
+                        0px 0px 8px 8px rgba(255, 255, 255, 1);
+            transition: transform 0.3s, box-shadow 0.3s;
+        }
+        .home_pic img {
+            width: 100%;
+            display: block;
+        }
+        .home_pic:hover {
+            transform: translateY(-5px);
+            box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2), 
+                        0 12px 24px rgba(0, 0, 0, 0.2);
+        }
     </style>
 </head>
 <body>
@@ -42,12 +59,12 @@
             @endforeach
         </div>
         <div class="home_pic chara_pic">
-        @if($latestIllustration)
-            <img src="{{ asset($latestIllustration->image_url) }}" alt="Illustration Image" />
-        @else
-            <img src="{{ asset($defaultImage) }}" alt="Default Image" />
-        @endif
-    </div>
+            @if($latestIllustration)
+                <img src="{{ asset($latestIllustration->image_url) }}" alt="Illustration Image" />
+            @else
+                <img src="{{ asset($defaultImage) }}" alt="Default Image" />
+            @endif
+        </div>
     </div>
     <div class="home_select">
         <div class="home_select_u">
