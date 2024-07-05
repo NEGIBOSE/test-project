8274b7c513745707365c2850727c35bbb2ec81d5
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-07-05 20:53:07 +0900

    整頓

--- resources/views/home/growth.blade.php
+++ resources/views/home/growth.blade.php
@@ -11,8 +11,6 @@
       href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap"
       rel="stylesheet"
     />
-
-    <!-- CSSファイルの読み込み -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
     <style>
       .logout button{
@@ -30,12 +28,10 @@
         display: flex;
         flex-wrap: wrap; /* コンテンツが折り返されるように設定 */
       }
-
       .growth_cont {
         margin: 5px; /* コンテンツの間隔を設定 */
         box-sizing: border-box; /* マージンやパディングを含めた全体の幅を計算する */
       }
-
       .growth_bottom {
         width:100%;
         display:flex;
@@ -43,8 +39,7 @@
       }
       .select_text{
         width:auto;
-      }
-            
+      }  
       .title a{
         background: none;
         padding: 0;
@@ -55,41 +50,39 @@
     </style>
   </head>
   <body>
-  <header class="header bg_green">
-    <div class="title">
-        <a href="{{ route('home.index') }}">
-            <img src="/images/chara_logo.png" alt="png Image">
-        </a>
-    </div>
-    <div class="logout">
-        <form action="{{ route('logout') }}" method="POST">
-            @csrf
-            <button type="submit">
-                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z"/></svg>
-                
-            </button>
-        </form>
-    </div>
-</header>
+    <header class="header bg_green">
+      <div class="title">
+          <a href="{{ route('home.index') }}">
+              <img src="/images/chara_logo.png" alt="png Image">
+          </a>
+      </div>
+      <div class="logout">
+          <form action="{{ route('logout') }}" method="POST">
+              @csrf
+              <button type="submit">
+                  <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z"/></svg>
+                  
+              </button>
+          </form>
+      </div>
+    </header>
     <main class="growth bg_green">
       <div class="growth_upper">
         
       </div>
       <div class="growth_middle bg_gray">
-      <ul class="growth_contents">
-    @foreach ($illustrations as $illustration)
-    <li class="growth_cont chara_pic">
-        <img src="{{ $illustration->image_url }}" alt="Illustration">
-    </li>
-    @endforeach
-</ul>
-
+        <ul class="growth_contents">
+          @foreach ($illustrations as $illustration)
+          <li class="growth_cont chara_pic">
+              <img src="{{ $illustration->image_url }}" alt="Illustration">
+          </li>
+          @endforeach
+        </ul>
       </div>
       <div class="bookshelf_bottom">
         <div class="select_text bg_green">
               <a href="{{ route('home.index') }}">HOMEへ戻る</a>
         </div>
-
       </div>
     </main>
     <footer>&copy; 2024 My portfolio</footer>
