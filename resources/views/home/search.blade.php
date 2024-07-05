ecd9c74ebb220a4f3c46c295bc1210fbfee1356b
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-07-05 21:30:39 +0900

    logoutボタン

--- resources/views/home/search.blade.php
+++ resources/views/home/search.blade.php
@@ -168,7 +168,7 @@
         <form action="{{ route('logout') }}" method="POST">
           @csrf
           <button type="submit">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" height="36px" viewBox="0 -960 960 960" width="36px"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z"/></svg>
           </button>
         </form>
       </div>
