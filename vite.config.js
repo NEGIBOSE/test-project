c7ad5b835b5e9ebd29cb7a5b13c527a1ecbcf996
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-07-23 13:37:36 +0900

    Fix Vite config to bind to Heroku port

--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,8 @@ export default defineConfig({
     server: {
         port: process.env.PORT || 3000,
         host: '0.0.0.0'
-    }
+    },
+    build: {
+        outDir: 'public/build',
+    },
 });
