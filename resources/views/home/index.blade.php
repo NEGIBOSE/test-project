2aa9649be0c0615d4468d55aeb1d9fddfacddda4
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-08-12 23:23:53 +0900

    heroku上の名前

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,7 +8,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet" />
     <!-- <link rel="stylesheet" href="{{ asset('css/app.css') }}" /> -->
-    <link rel="stylesheet" href="{{ secure_asset('build/assets/app-DmdGSnKO.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('build/assets/app-DVRyBTKm.css') }}">
 
     <style>
         .home_icon {
