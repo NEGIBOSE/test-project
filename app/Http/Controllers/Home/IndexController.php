b14b324bfb59ac730e98c021558f3948013f5733
NEGIBOSE <ryo0827ngsh@icloud.com>
2024-08-21 22:55:29 +0900

    login状態記録

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -10,21 +10,30 @@ use App\Models\Illustration; // Illustration モデルをインポート
 
 class IndexController extends Controller
 {
-    function index()
+    public function index()
     {
-        // ここで生成されるURLを確認
-        // dd(secure_url(route('logout')));
-        
         // ログインユーザーの取得
         $user = auth()->user();
+
+        if (!$user) {
+            // ユーザーがログインしていない場合の処理
+            return redirect()->route('login')->with('error', 'ログインが必要です');
+        }
 
         // ユーザーに関連する最新のIllustrationデータを取得
         $latestIllustration = Illustration::where('user_id', $user->id)->latest()->first();
 
+        // カテゴリの取得
         $categories = Category::all();
         
         // Illustrationデータが存在しない場合はデフォルトの画像パスを設定
         $defaultImage = 'images/BABY.png';
-        return view('home.index', compact('categories', 'latestIllustration', 'defaultImage'));
+
+        // ビューにデータを渡す
+        return view('home.index', [
+            'categories' => $categories,
+            'latestIllustration' => $latestIllustration ?? (object) ['image_url' => $defaultImage], // Illustrationがnullの場合はデフォルト画像を設定
+            'defaultImage' => $defaultImage
+        ]);
     }
 }
